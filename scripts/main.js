1911025ac1b9883ddb4e01ab89772e1015c62412
Iminguet <i.minguet@hotmail.com>
2022-09-23 13:30:35 +0200

    Se crea el archivo colores.js para almacenar las variables de los colores. Es de modo temporal mientras se ordena el código.

--- scripts/main.js
+++ scripts/main.js
@@ -1,98 +1,5 @@
 'use-strict';
 
-//colores como objeto. Revisar ejercicio mapeo del día 03
-
-//rojo
-const R1 = document.getElementById('R1');
-const R2 = document.getElementById('R2');
-const R3 = document.getElementById('R3');
-const R4 = document.getElementById('R4');
-const R5 = document.getElementById('R5');
-
-//naranja
-const N1 = document.getElementById('N1');
-const N2 = document.getElementById('N2');
-const N3 = document.getElementById('N3');
-const N4 = document.getElementById('N4');
-const N5 = document.getElementById('N5');
-
-//amarillo
-const A1 = document.getElementById('A1');
-const A2 = document.getElementById('A2');
-const A3 = document.getElementById('A3');
-const A4 = document.getElementById('A4');
-const A5 = document.getElementById('A5');
-
-//menta
-const M1 = document.getElementById('M1');
-const M2 = document.getElementById('M2');
-const M3 = document.getElementById('M3');
-const M4 = document.getElementById('M4');
-const M5 = document.getElementById('M5');
-
-//verde
-const V1 = document.getElementById('V1');
-const V2 = document.getElementById('V2');
-const V3 = document.getElementById('V3');
-const V4 = document.getElementById('V4');
-const V5 = document.getElementById('V5');
-
-//pistacho
-const P1 = document.getElementById('P1');
-const P2 = document.getElementById('P2');
-const P3 = document.getElementById('P3');
-const P4 = document.getElementById('P4');
-const P5 = document.getElementById('P5');
-
-//celeste
-const CE1 = document.getElementById('CE1');
-const CE2 = document.getElementById('CE2');
-const CE3 = document.getElementById('CE3');
-const CE4 = document.getElementById('CE4');
-const CE5 = document.getElementById('CE5');
-
-//cielo
-const C1 = document.getElementById('C1');
-const C2 = document.getElementById('C2');
-const C3 = document.getElementById('C3');
-const C4 = document.getElementById('C4');
-const C5 = document.getElementById('C5');
-
-//azul
-const AZ1 = document.getElementById('AZ1');
-const AZ2 = document.getElementById('AZ2');
-const AZ3 = document.getElementById('AZ3');
-const AZ4 = document.getElementById('AZ4');
-const AZ5 = document.getElementById('AZ5');
-
-//lila
-const L1 = document.getElementById('L1');
-const L2 = document.getElementById('L2');
-const L3 = document.getElementById('L3');
-const L4 = document.getElementById('L4');
-const L5 = document.getElementById('L5');
-
-//morado
-const MO1 = document.getElementById('MO1');
-const MO2 = document.getElementById('MO2');
-const MO3 = document.getElementById('MO3');
-const MO4 = document.getElementById('MO4');
-const MO5 = document.getElementById('MO5');
-
-//rosa
-const RO1 = document.getElementById('RO1');
-const RO2 = document.getElementById('RO2');
-const RO3 = document.getElementById('RO3');
-const RO4 = document.getElementById('RO4');
-const RO5 = document.getElementById('RO5');
-
-//oscuro
-const O1 = document.getElementById('O1');
-const O2 = document.getElementById('O2');
-const O3 = document.getElementById('O3');
-const O4 = document.getElementById('O4');
-const O5 = document.getElementById('O5');
-
 // Inicio programa
 const ladoCuadrado = 32;
 const colorCuadrados = 'white';
@@ -104,38 +11,38 @@ const cantidadAlto = parseInt(alto / ladoCuadrado);
 const cantidadCuadrados = cantidadAlto * cantidadAncho;
 
 const drag = (ev) => {
-  ev.dataTransfer.setData('text', ev.target.id);
-  console.log('arrastrando...', ev.target.id);
+    ev.dataTransfer.setData('text', ev.target.id);
+    console.log('arrastrando...', ev.target.id);
 };
 const allowDrop = (ev) => {
-  ev.preventDefault();
+    ev.preventDefault();
 };
 
 const drop = (ev) => {
-  ev.preventDefault(ev);
-  let data = ev.dataTransfer.getData('text');
-  ev.target.appendChild(document.getElementById(data));
+    ev.preventDefault(ev);
+    let data = ev.dataTransfer.getData('text');
+    ev.target.appendChild(document.getElementById(data));
 };
 const nombreCuadrado = 'cuadradoUnitarioNº';
 let numeroCuadrado = 0;
 let nombreDefinitivo = `${nombreCuadrado}${numeroCuadrado}`;
 
 for (let i = 0; i < cantidadCuadrados; i++) {
-  let cuadrado = document.createElement('div');
-  cuadrado.className = 'estiloCuadrado';
-  cuadrado.style.cssText = `width:${ladoCuadrado}px;height: ${ladoCuadrado}px`;
-  numeroCuadrado++;
-  nombreDefinitivo = `${nombreCuadrado}${numeroCuadrado}`;
-  cuadrado.setAttribute('id', `${nombreDefinitivo}`);
-  cuadrado.setAttribute('draggable', 'true');
-  cuadrado.setAttribute('ondragstart', 'drag(event)');
-  container.appendChild(cuadrado);
-  for (let j = 0; j < 4; j++) {
-    let cuadraditos = document.createElement('div');
-    cuadrado.appendChild(cuadraditos);
-    cuadraditos.classList.add('cuadradosInteriores');
-    cuadraditos.setAttribute('draggable', 'true');
-  }
+    let cuadrado = document.createElement('div');
+    cuadrado.className = 'estiloCuadrado';
+    cuadrado.style.cssText = `width:${ladoCuadrado}px;height: ${ladoCuadrado}px`;
+    numeroCuadrado++;
+    nombreDefinitivo = `${nombreCuadrado}${numeroCuadrado}`;
+    cuadrado.setAttribute('id', `${nombreDefinitivo}`);
+    cuadrado.setAttribute('draggable', 'true');
+    cuadrado.setAttribute('ondragstart', 'drag(event)');
+    container.appendChild(cuadrado);
+    for (let j = 0; j < 4; j++) {
+        let cuadraditos = document.createElement('div');
+        cuadrado.appendChild(cuadraditos);
+        cuadraditos.classList.add('cuadradosInteriores');
+        cuadraditos.setAttribute('draggable', 'true');
+    }
 }
 console.log(ancho);
 console.log(alto);
