5127b0e00c1d2f1b83dafeac21fd84d70148062a
Iminguet <i.minguet@hotmail.com>
2022-09-25 16:21:28 +0200

    La creación de la cuadricula ha sido refactorizada en funciones con parametros y argumentos.

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,59 @@
 'use-strict';
 
 // Inicio programa
-
 // asignacion de variables
-const ladoCuadrado = 32; // TODO añadir user input y pasar valor por defecto 32px
 const container = document.getElementById('cuadricula');
-const ancho = container.scrollWidth;
-const alto = container.scrollHeight;
-const cantidadAncho = parseInt(ancho / ladoCuadrado);
-const cantidadAlto = parseInt(alto / ladoCuadrado);
-const cantidadCuadrados = cantidadAlto * cantidadAncho;
 
-// eventos de mouse
-const drag = (ev) => {
-    ev.dataTransfer.setData('text', ev.target.id);
-    console.log('arrastrando...', ev.target.id);
-};
-const allowDrop = (ev) => {
-    ev.preventDefault();
-};
+const defaultLadoCuadrado = 32;
+const ladoCuadrado = undefined; // TODO añadir user input y pasar valor por defecto 32px
+// Dato del user input bajo la cuadricula de colores
 
-const drop = (ev) => {
-    ev.preventDefault(ev);
-    let data = ev.dataTransfer.getData('text');
-    ev.target.appendChild(document.getElementById(data));
+// entra el numero de px por cubo
+// devuelve el tamaño de la cuadricula
+const cantidadTotal = (lPx = defaultLadoCuadrado) => {
+    console.log(lPx, 'Poje');
+    const ancho = container.scrollWidth;
+    const alto = container.scrollHeight;
+    const cantidadAncho = parseInt(ancho / lPx);
+    const cantidadAlto = parseInt(alto / lPx);
+    const cantidadCuadrados = cantidadAlto * cantidadAncho;
+    return cantidadCuadrados;
 };
 
+const totales = cantidadTotal(ladoCuadrado);
+console.log(totales);
 // Asignamos ID a cada cuadrado
-const nombreCuadrado = 'cuadradoUnitarioNº';
-let numeroCuadrado = 0;
-let nombreDefinitivo = `${nombreCuadrado}${numeroCuadrado}`;
 
+// entra el número de id y el tamaño del lado
+// devuelve el nombre del cuadrado
+const creacionCuadrados = (nº, lPx = defaultLadoCuadrado) => {
+    let cuadrado = document.createElement('div');
+    cuadrado.className = 'estiloCuadrado';
+    cuadrado.style.cssText = `width:${lPx}px;height: ${lPx}px`;
+    const nombreCuadrado = 'cuadradoUnitarioNº';
+    const nombreDefinitivo = `${nombreCuadrado}${nº}`;
+    cuadrado.id = `${nombreDefinitivo}`;
+    cuadrado.setAttribute('draggable', 'true');
+    cuadrado.setAttribute('ondragstart', 'drag(event)');
+    container.appendChild(cuadrado);
+    //
+    //
+    //
+    return nombreDefinitivo;
+};
+
+const creacionCuadraditos = (idCuadradoGrande, iteracion) => {
+    let cuadrado = document.getElementById(`${idCuadradoGrande}`);
+    let cuadradoId = cuadrado.id;
+    console.log(cuadradoId);
+    const arrNombre = cuadradoId.split('cuadradoUnitario');
+    let cuadraditos = document.createElement('div');
+    cuadraditos.id = arrNombre[1] + iteracion;
+    cuadrado.appendChild(cuadraditos);
+    cuadraditos.className = 'cuadradosInteriores';
+    cuadraditos.setAttribute('draggable', 'true');
+    cuadraditos.setAttribute('ondrop', 'drop(event)');
+};
 /*
 Pintamos cuadrados del tamaño dado con la const ladoCuadrado 
 con un bucle for, durante toda la pantalla
@@ -38,23 +61,20 @@ Aquí tb asigamos clases. id y pintamos cuadrados dentro de cuadrados.
 
 /// TODO, refactorizar esto con funciones /// 
 
+
 */
-for (let i = 0; i < cantidadCuadrados; i++) {
-    let cuadrado = document.createElement('div');
-    cuadrado.className = 'estiloCuadrado';
-    cuadrado.style.cssText = `width:${ladoCuadrado}px;height: ${ladoCuadrado}px`;
-    numeroCuadrado++;
-    nombreDefinitivo = `${nombreCuadrado}${numeroCuadrado}`;
-    cuadrado.setAttribute('id', `${nombreDefinitivo}`);
-    cuadrado.setAttribute('draggable', 'true');
-    cuadrado.setAttribute('ondragstart', 'drag(event)');
-    container.appendChild(cuadrado);
-    for (let j = 0; j < 4; j++) {
-        let cuadraditos = document.createElement('div');
-        cuadrado.appendChild(cuadraditos);
-        cuadraditos.classList.add('cuadradosInteriores');
-        cuadraditos.setAttribute('draggable', 'true');
+
+// entra la cantidad de cuadrados que forma la cuadricula ** cantidadTotal(xx)**
+// devulve cuadricula creada
+
+const creacionCuadricula = (totales, lPx) => {
+    for (let i = 0; i < totales; i++) {
+        const id = creacionCuadrados(i, lPx);
+
+        for (let j = 0; j < 4; j++) {
+            creacionCuadraditos(id, j);
+        }
     }
-}
-console.log(ancho);
-console.log(alto);
+};
+
+creacionCuadricula(totales, ladoCuadrado);
